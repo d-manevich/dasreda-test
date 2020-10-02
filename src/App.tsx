e49b4696a00145bb3ec4fd510e9f22216d135c20
Daniil Manevich <daniil.manevich@gmail.com>
2020-10-02 10:04:28 +0300

    Add access token request, show top 10 js repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query jsRepos {
+    search(query: "created:>2020-09-02 language:JavaScript sort:stars-desc", type: REPOSITORY, first: 10) {
+      repositoryCount
+      nodes {
+        ... on Repository {
+          id
+          name
+          owner {
+            avatarUrl
+          }
+          description
+          stargazerCount
+          url
+        }
+      }
+    }
+  }
+`;
 
 const App: React.FC = () => {
-  return <div className="App">Test</div>;
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.search.nodes.map(({ id, name }: { id: string; name: string }) => (
+    <div key={id}>
+      <p>{name}</p>
+    </div>
+  ));
 };
 
 export default App;
